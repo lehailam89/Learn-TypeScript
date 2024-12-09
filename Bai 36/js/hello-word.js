3dd40d25b8100de25ec86036f05b14d8a76f3177
Lâm <lehailamltk@gmail.com>
2024-12-09 22:06:41 +0700

    hoc typescript video 46

--- Bai 36/js/hello-word.js
+++ Bai 36/js/hello-word.js
@@ -1,29 +1,22 @@
+"use strict";
 //Phần 1
 var hello = "Hello World!";
 console.log(hello);
-
 //Phần 3
 //3.1. Kiểu number
-let a:number = 10;
-let b:number = 10.5;
-
+let a = 10;
+let b = 10.5;
 //a = "xin chao"; lỗi không thể gán kiểu string cho biến kiểu number trong TypeScript
-
 a = 20; //không lỗi vì a là kiểu number và giá trị gán cho a cũng là kiểu number
-
 console.log(typeof a);
 console.log(typeof b);
 //3.2. Kiểu string
-let fullName:string = "Nguyen Van A";
+let fullName = "Nguyen Van A";
 fullName = "Le Van A";
-
 console.log(fullName);
 console.log(typeof fullName);
-
 //3.3. Kiểu boolean
-let deleted:boolean = false;
-
+let deleted = false;
 deleted = true;
-
 console.log(deleted);
 console.log(typeof deleted);
